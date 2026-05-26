26168f5dab05708dcb3d72060f42e29ba5bfe418
yedidya ben haim <bhyedidya@gmail.com>
2026-05-26 16:40:40 +0300

    logging - solution 1-2

---
+++ week_7/logging.py
@@ -0,0 +1,26 @@
+# region Exercise 1
+#
+# 1. X
+# 2. V
+# 3. V
+# 4. X
+# 5. V
+# 6. V
+# 7. X
+
+# endregion
+
+# region Exercise 2
+
+# 1. INFO
+# 2. ERROR
+# 3. DEBUG
+# 4. ERROR
+# 5. WARNING
+# 6. INFO
+
+# endregion
+
+# region Exercise 3
+
+# log a
