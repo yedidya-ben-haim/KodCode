5c14f9b77c1f045562ab4633ea4e3ddf378e7e10
yedidya ben haim <bhyedidya@gmail.com>
2026-06-23 17:38:48 +0300

    Solution Week_11 | Day_3 | PART 2

--- week_11/Day_3/exercises/Program.cs
+++ week_11/Day_3/exercises/Program.cs
@@ -1,15 +1,13 @@
-﻿
-
-namespace project_day_2
+﻿namespace project_day_2
 {
     class Exercise
     {
+        enum ClassificationStatus { Friendly = 1, Hostile = 2, Nunidentified = 3 }
 
-        static void Main()
+
+        static void PrintMenu()
         {
-            static void PrintMenu()
-            {
-                Console.WriteLine("""
+            Console.WriteLine("""
                     ====================
                     = Signal Intercept =
                     ====================
@@ -20,37 +18,164 @@ namespace project_day_2
 
                     Choose option (1-4)
                     """);
+        }
 
-                
+
+        static int GetId()
+        {
+            while (true)
+            {
+                Console.Write("Please enter source ID: ");
+                string idInput = Console.ReadLine();
+
+                if (int.TryParse(idInput, out int validId))
+                {
+                    return validId;
+                }
+                else
+                {
+                    Console.WriteLine("Error: ID must be number. Try again.");
+                }
             }
+        }
+
+
+        static double? GetNewSignalStrength()
+        {
+            while (true)
+            {
+                Console.Write("Please enter source signal strength: ");
+                string signalStrengthInput = Console.ReadLine();
+
+                // The user did not enter anything.
+                if (string.IsNullOrEmpty(signalStrengthInput))
+                {
+                    return null;
+                }
+
+                if (double.TryParse(signalStrengthInput, out double validStrengthInput))
+                {
+                    return validStrengthInput;
+                }
+                else
+                {
+                    Console.WriteLine("Error: signal strength must be double number. Try again.");
+                }
+            }
+        }
+
+
+        static ClassificationStatus GetNewClassification()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter source Classification:");
+                Console.WriteLine("""
+                                    1 = Friendly
+                                    2 = Hostile
+                                    3 = Unidentified
+                                    """);
+                Console.Write("Choose option (1-3): ");
+
+                string classificationInput = Console.ReadLine();
+
+
+                if (int.TryParse(classificationInput, out int validClassification)
+                    && validClassification >= 1 && validClassification <= 3)
+                {
+
+                    return (ClassificationStatus)validClassification;
+                }
+                else
+                {
+
+                    Console.WriteLine("Invalid selection. Please enter 1, 2, or 3.");
+                }
+            }
+        }
+
+
+
+        static void ShowAll(List<int> Ids, List<double?> signalStrengthList, List<ClassificationStatus> classificationList)
+        {
+
+            Console.WriteLine("=======all Signal Intercept========== ");
+
+
+            for (int i = 0; i < Ids.Count; i++)
+            {
+
+                string signalStrength = signalStrengthList[i]?.ToString()?? "unknown";
+
+
+                Console.WriteLine($"""
+                    \nId: {Ids[i]}
+                    Signal strength: {signalStrength}
+                    Classification status: {classificationList[i]}
+                    """);
+            }
+        }
+
+
+
+        static void Main()
+        {
+            List<int> IdList = new();
+            List<double?> signalStrengthList = new();
+            List<ClassificationStatus> classificationList = new();
 
 
             bool keepRunning = true;
 
             while (keepRunning)
             {
-                Console.WriteLine(PrintMenu);
+                PrintMenu();
 
                 string userInput = Console.ReadLine();
 
-                
+
                 if (int.TryParse(userInput, out int input))
                 {
                     switch (input)
                     {
                         case 1:
-                            Console.WriteLine("1");
+                            int newId = GetId();
+                            ClassificationStatus newClassification = GetNewClassification();
+                            double? newSignalStrength = GetNewSignalStrength();
+
+                            IdList.Add(newId);
+                            classificationList.Add(newClassification);
+                            signalStrengthList.Add(newSignalStrength);
+                            break;
+                        
+                        case 2:
+                            int inputId = GetId();
+                            int index = IdList.IndexOf(inputId);
+                            if (index >= 0)
+                            {
+                                double? inputSignalStrength = GetNewSignalStrength();
+                                signalStrengthList[index] = inputSignalStrength;
+                            }
+                            else
+                            {
+                                Console.WriteLine("The id does not exist.");
+                            }
+                            break;
+
+                        case 3:
+                            ShowAll(IdList, signalStrengthList, classificationList);
                             break;
 
                         case 4:
                             Console.WriteLine("Exit");
                             keepRunning = false;
                             break;
-                        
 
-
+                        default:
+                            Console.WriteLine("Please enter a number between 1-4");
+                            break;
                     }
-                   
+
                 }
             }
 
