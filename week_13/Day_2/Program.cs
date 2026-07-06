938f83c08f75d3b1a29c0face4fc3f960869803f
yedidya ben haim <bhyedidya@gmail.com>
2026-07-06 12:44:01 +0300

    Coding Exercise| day_1 | part 1

---
+++ week_13/Day_2/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace project
+{
+    class project_day_1
+    {
+        class ImageMetadataManagerBadVersion
+        {
+            public int Id { set; get; };
+            public double CloudCover { set; get; };
+            public string Sensor { set; get; };
+
+            public bool IsValid()
+            {
+                if (CloudCover < 0 && CloudCover > 100)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            public string Format()
+            {
+                return $"Image {Id}:{CloudCover} cloud {Sensor}."
+            }
+
+            void SaveToFile(ImageMetadataManagerBadVersion i, string path)
+            {
+                File.WriteAllText(path, i.Format());
+            }
+
+            public int Score(ImageMetadataManagerBadVersion i)
+            {
+                int SensorScore = 0;
+
+                switch (i.Sensor)
+                {
+                    case "SAR":
+                        SensorScore = 100;
+                        break;
+
+                    case "EO":
+                        SensorScore = 60;
+                        break;
+
+                    case "IR":
+                        SensorScore = 40;
+                        break;
+
+                    default:
+                        SensorScore = 0;
+                        break;
+                }
+                return SensorScore - (int)i.CloudCover;
+            }
+
+
+
+
+
+        }
+    }
+}
